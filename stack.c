13f8c23586351524cef91af1153d989dae4f089f
Loay Mohamed <72279810+zoroxide@users.noreply.github.com>
2024-06-01 12:51:28 +0300

    init

--- stack.c
+++ stack.c
@@ -16,3 +16,39 @@ return_statue_t stack_init(Stack * my_stack) {
     }
     return ret;
 }
+
+static stack_status_t stack_full(const Stack * mystack) {
+    if(mystack->stack_pointer == (STACK_MAX_SIZE - 1)) {
+        return STACK_FULL;
+    }else {
+        return STACK_NOT_FULL;
+    }
+}
+
+static stack_status_t stack_empty(const Stack * mystack) {
+    if(mystack->stack_pointer == -1) {
+        return STACK_EMPTY;
+    }else {
+        return stack_full(mystack);
+    }
+}
+
+stack_status_t stack_push(const Stack * mystack, uint32_t value) {
+
+}
+
+stack_status_t stack_pop(const Stack * mystack, uint32_t *value) {
+
+}
+
+stack_status_t stack_top(const Stack * mystack, uint32_t *value) {
+
+}
+
+stack_status_t stack_size(const Stack * mystack, uint32_t *_size) {
+
+}
+
+stack_status_t stack_display(const Stack * mystack) {
+
+}
--- stack.h
+++ stack.h
@@ -2,7 +2,6 @@
 // Created by loay on 5/26/2024.
 //
 
-
 #ifndef STACK_H
 #define STACK_H
 #include "std_types.h"
@@ -14,13 +13,29 @@ typedef struct stack_ds {
     uint32_t data[STACK_MAX_SIZE];
 } Stack;
 
+typedef enum stack_statue {
+ STACK_FULL,
+ STACK_EMPTY,
+ STACK_NOT_FULL
+} stack_status_t;
 
-/*
- * @brief: initialize the Stack and checking of any null pointers
- * @params: an adress of your desired Stack
- * @retval: R_NOT if the stack initialization field, ROK is it success.
- */
+
 return_statue_t stack_init(Stack * my_stack);
+
+static stack_status_t stack_full(const Stack * mystack);
+
+static stack_status_t stack_empty(const Stack * mystack);
+
+stack_status_t stack_push(const Stack * mystack, uint32_t value);
+
+stack_status_t stack_pop(const Stack * mystack, uint32_t *value);
+
+stack_status_t stack_top(const Stack * mystack, uint32_t *value);
+
+stack_status_t stack_size(const Stack * mystack, uint32_t *_size);
+
+stack_status_t stack_display(const Stack * mystack);
+
 
 
 #endif //STACK_H
